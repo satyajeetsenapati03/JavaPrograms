adf00e222282ddea25eb36d07e2a5217f4b4184f
Satyajeet <satyajeetsenapati03@gmail.com>
2024-02-24 00:28:46 +0530

    modified successfully

--- StringProgram/src/B.java
+++ StringProgram/src/B.java
@@ -1,5 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class B {
     public static void main(String[] args) {
-        System.out.println("hello");
+        String s = "hello world";
+        String[] rev = s.split(" ");
+      for(int i=rev.length-1;i>=0;i--){
+          System.out.print(rev[i]+" ");
+      }
     }
 }
